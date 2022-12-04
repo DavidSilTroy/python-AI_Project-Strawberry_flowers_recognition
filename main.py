a39c6f184fd98f014c0221dd66aac3600a0e2288
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-12-04 11:51:41 +0100

    Another beautiful day to train an AI till perfection!

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.header('Thomas More - Artificial Intelligence Project: Object detection')
+
+st.write("Here is going to be an Epic/Awesome Object Detection program!")
+
+body = st.container()
+body.write("Coming soon.... (in Winter)")
